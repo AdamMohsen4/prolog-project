b1ded2736014c7669d04a9352850331623eb593a
Adam Abdulmohsen <adam.amohsen4@gmail.com>
2025-10-02 23:42:32 +0200

    adds implementation of remove_duplicates predicate in Prolog

---
+++ uppg2.pl
@@ -0,0 +1,10 @@
+% Fall 1: Listan är tom
+remove_duplicates([], []).
+
+% Fall 2: Om huvudet (H) finns i resten av listan (T), 
+% är det en dubblett - skippa H och fortsätt med resten
+remove_duplicates([H|T], E) :- member(H, T), remove_duplicates(T,E).
+
+% Fall 3: Om huvudet (H) INTE finns i resten av listan (T),
+% behåll H i resultatet och fortsätt med resten
+remove_duplicates([H|T], [H|E]) :- \+ member(H, T), remove_duplicates(T,E).
