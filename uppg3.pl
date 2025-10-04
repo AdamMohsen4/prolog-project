cea063010e6a32ed47be23ff89c9119fed9f2f06
Adam Abdulmohsen <adam.amohsen4@gmail.com>
2025-10-04 15:07:00 +0200

    Finalizes partsring for case where head is not part of substring

--- uppg3.pl
+++ uppg3.pl
@@ -4,9 +4,16 @@ my_length([], 0).
 my_length([_|T], N) :- my_length(T, N1), N is N1 + 1.
 
 
+% base case
 partstring([], 0, []).
 
+% if the head of the list is part of substring, we include
 partstring([H|T], L, F) :- 
     append(F, _, [H|R]),
     F \= [],
     my_length(R, L).
+
+
+% if the heaed of the list is not part of the substring, we skip 
+partstring([_|T], L, F) :-
+    partstring(T, L, F).
