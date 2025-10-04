d56d7f0f7e0d11c61d847bf41bc2030b354754a0
Adam Abdulmohsen <adam.amohsen4@gmail.com>
2025-10-04 15:28:23 +0200

    adds a graph

--- uppg4.pl
+++ uppg4.pl
@@ -1,8 +1,19 @@
 
 
+edge(a, b).
+edge(a, c).
+edge(b, d).
+edge(c, d).
+edge(b, e).
+edge(d, f).
+edge(a,f).
+
 path(Start, End, Path) 
   % something here
 
 
-path_helper(Current, End, Visited, Path) 
-  
+path_helper(Current, End, Visited, Path) :-
+     Current = End,
+     Path = Visited.
+
+path_helper(Current, End, Visited, Path) :-
