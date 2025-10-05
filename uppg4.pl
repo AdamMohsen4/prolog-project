3a604a15cd5833e2a748bbf37067f459bf1e0fc9
Adam Abdulmohsen <adam.amohsen4@gmail.com>
2025-10-05 17:58:57 +0200

    change documentation

--- uppg4.pl
+++ uppg4.pl
@@ -1,32 +1,32 @@
 
-% Helper predicates
+% Hjälppredikat
 append([], L, L).
 append([H|T], L, [H|R]) :- append(T, L, R).
 select(X, [X|T], T).
 select(X, [Y|T], [Y|R]) :- select(X, T, R).
 member(X, L) :- select(X, L, _).
 
-% Graph edges
+% Graf definition
 edge(a, b).
 edge(a, c).
 edge(b, d).
 edge(c, d).
 edge(b, e).
 edge(d, f).
-edge(a,f).
+edge(a, f).
 
-% Main predicate to find path from Start to End
+% Huvudpredikat
 path(Start, End, Path) :-
     path_helper(Start, End, [Start], Path).
 
-% case: if current node is the end node, we are done
+% fall: om nuvarande nod är slutnoden, är vi klara (och returnerar sökvägen)
 path_helper(Current, End, Visited, Path) :-
      Current = End,
      Path = Visited.
 
-% recursive case: explore neighbors (similar to DFS)
+% rekursivt fall: utforska grannar (liknande DFS)
 path_helper(Current, End, Visited, Path) :-
       edge(Current, Next),
-      \+ member(Next, Visited),
+      \+ member(Next, Visited), % om nästa nod inte har besökts
       append(Visited, [Next], NewVisited),
       path_helper(Next, End, NewVisited, Path).
